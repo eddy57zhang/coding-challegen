09b2a6e8d0844ebd837ca69c66ac73af6739670b
eddy57zhang <zhongchuanzhang@gmail.com>
2015-11-05 23:25:48 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #The first feature: clean and extract the text from the raw JSON tweets that come from the Twitter Streaming API, and track the number of tweets that contain unicode.
-python ./src/cleanTweets.py ./tweet_input/input2.txt ./tweet_output/ft1.txt
+python ./src/cleanTweets.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
 #The second feature: calculate the average degree of a vertex in a Twitter hashtag graph for the last 60 seconds, and update this each time a new tweet appears.
-python ./src/averageDegree.py ./tweet_input/input2.txt ./tweet_output/ft2.txt
+python ./src/averageDegree.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
